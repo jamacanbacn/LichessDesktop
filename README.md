b1568077d34a62dc9df765705e043267f682cd8c
jamacanbacn <jamacanbacn@gmail.com>
2023-03-20 06:04:54 -0700

    Initial commit
    Barely works will fix icon later

---
+++ README.md
@@ -0,0 +1,3 @@
+Not affiliated with Lichess in anyway make sure to support them at https://lichess.org
+
+Using https://tauri.app/
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,37 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn main() -> wry::Result<()> {
+  use wry::{
+    application::{
+      event::{Event, StartCause, WindowEvent},
+      event_loop::{ControlFlow, EventLoop},
+      window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+  };
+
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+    .with_title("Lichess Desktop")
+    .build(&event_loop)?;
+  let _webview = WebViewBuilder::new(window)?
+    .with_url("https://lichess.org")?
+    .build()?;
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
+
+}
